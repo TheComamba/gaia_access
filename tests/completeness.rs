39c8c956ac54901d1511095bd2c61bb6472fa9d0
TheComamba <thecomamba@jojoheinze.de>
2024-03-09 09:07:56 +0100

    Reading gaia tables.

--- tests/completeness.rs
+++ tests/completeness.rs
@@ -7,6 +7,12 @@ mod tests {
 
     const FILE_PATH: &'static str = "dev_data/tables.xml";
 
+    struct GaiaTables {
+        schemas: Vec<String>,
+        tables: Vec<String>,
+        columns: Vec<String>,
+    }
+
     fn check_and_download_file() -> Result<(), GaiaError> {
         if !fs::metadata(FILE_PATH).is_ok() {
             fs::create_dir_all("dev_data").map_err(|e| GaiaError::General(e.to_string()))?;
@@ -22,23 +28,60 @@ mod tests {
         Ok(())
     }
 
-    fn read_xml_file() -> Result<(), GaiaError> {
+    fn read_xml_file() -> Result<GaiaTables, GaiaError> {
         check_and_download_file()?;
         let file = fs::File::open(FILE_PATH).map_err(|e| GaiaError::General(e.to_string()))?;
 
+        let mut gaia_tables = GaiaTables {
+            schemas: Vec::new(),
+            tables: Vec::new(),
+            columns: Vec::new(),
+        };
+
         let reader = BufReader::new(file);
         let root = Element::parse(reader).map_err(|e| GaiaError::General(e.to_string()))?;
-        println!("Root has {} children", root.children.len());
         for schema in root.children {
             let schema = schema.as_element().unwrap();
-            println!(
-                "Schema {} has {} children",
-                schema.name,
-                schema.children.len()
-            );
+            let schema_name = schema.get_child("name").unwrap().children[0]
+                .as_text()
+                .unwrap();
+            gaia_tables.schemas.push(schema_name.to_string());
+            for table in &schema.children {
+                let table = table.as_element().unwrap();
+                if table.name != "table" {
+                    continue;
+                }
+                let table_name = table.get_child("name").unwrap().children[0]
+                    .as_text()
+                    .unwrap();
+                let table_name_parts: Vec<&str> = table_name.split('.').collect();
+                let table_name = table_name_parts.last().unwrap();
+                gaia_tables.tables.push(table_name.to_string());
+                for column in &table.children {
+                    let column = column.as_element().unwrap();
+                    if column.name != "column" {
+                        continue;
+                    }
+                    let column_name = column.get_child("name").unwrap().children[0]
+                        .as_text()
+                        .unwrap();
+                    gaia_tables.columns.push(column_name.to_string());
+                }
+            }
         }
 
-        Ok(())
+        gaia_tables.schemas.sort();
+        gaia_tables.schemas.dedup();
+        gaia_tables.tables.sort();
+        gaia_tables.tables.dedup();
+        gaia_tables.columns.sort();
+        gaia_tables.columns.dedup();
+
+        println!("Schemas: {:?}", gaia_tables.schemas.len());
+        println!("Tables: {:?}", gaia_tables.tables.len());
+        println!("Columns: {:?}", gaia_tables.columns.len());
+
+        Ok(gaia_tables)
     }
 
     #[test]
