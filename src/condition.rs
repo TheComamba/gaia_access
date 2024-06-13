960ad14072ade89b1fe9095a13c0a9337f8ff56c
TheComamba <98019967+TheComamba@users.noreply.github.com>
2024-06-13 21:39:08 +0200

    Dev/more comparison operators (#13)

--- src/condition.rs
+++ src/condition.rs
@@ -10,19 +10,29 @@ pub enum GaiaCondition<C: Column> {
     Between(C, f64, f64),
     /// GreaterThan(column, threshold) is rendered as "column > threshold" in ADQL.
     GreaterThan(C, f64),
+    /// GreaterThanOrEqual(column, threshold) is rendered as "column >= threshold" in ADQL.
+    GreaterThanOrEqual(C, f64),
     /// LessThan(column, threshold) is rendered as "column < threshold" in ADQL.
     LessThan(C, f64),
+    /// LessThanOrEqual(column, threshold) is rendered as "column <= threshold" in ADQL.
+    LessThanOrEqual(C, f64),
 }
 
 impl<C: Column> fmt::Display for GaiaCondition<C> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let str = match self {
-            GaiaCondition::LessThan(column, value) => format!("{} < {}", column.to_string(), value),
+            GaiaCondition::Between(column, value1, value2) => {
+                format!("{} BETWEEN {} AND {}", column.to_string(), value1, value2)
+            }
             GaiaCondition::GreaterThan(column, value) => {
                 format!("{} > {}", column.to_string(), value)
             }
-            GaiaCondition::Between(column, value1, value2) => {
-                format!("{} BETWEEN {} AND {}", column.to_string(), value1, value2)
+            GaiaCondition::GreaterThanOrEqual(column, value) => {
+                format!("{} >= {}", column.to_string(), value)
+            }
+            GaiaCondition::LessThan(column, value) => format!("{} < {}", column.to_string(), value),
+            GaiaCondition::LessThanOrEqual(column, value) => {
+                format!("{} <= {}", column.to_string(), value)
             }
         };
         write!(f, "({})", str)
@@ -41,9 +51,9 @@ mod tests {
     const NUMBER_OF_COLUMNS: usize = 20;
 
     #[test]
-    fn request_less_than_condition() {
+    fn request_between_condition() {
         let col = Col::ecl_lon;
-        let cond = GaiaCondition::LessThan(col, 10.0);
+        let cond = GaiaCondition::Between(col, 10.0, 20.0);
         let result = GaiaQueryBuilder::new(gaiadr3, gaia_source)
             .top(NUMBER_OF_COLUMNS)
             .select(vec![col])
@@ -52,7 +62,7 @@ mod tests {
             .unwrap();
         assert_eq!(result.columns[0], col);
         let ecl_lon = get_float(result.data[0].get(&col).unwrap()).unwrap();
-        assert!(ecl_lon < 10.0);
+        assert!(ecl_lon > 10.0 && ecl_lon < 20.0);
     }
 
     #[test]
@@ -71,9 +81,9 @@ mod tests {
     }
 
     #[test]
-    fn request_between_condition() {
+    fn request_greater_than_or_equal_condition() {
         let col = Col::ecl_lon;
-        let cond = GaiaCondition::Between(col, 10.0, 20.0);
+        let cond = GaiaCondition::GreaterThanOrEqual(col, 10.0);
         let result = GaiaQueryBuilder::new(gaiadr3, gaia_source)
             .top(NUMBER_OF_COLUMNS)
             .select(vec![col])
@@ -82,6 +92,36 @@ mod tests {
             .unwrap();
         assert_eq!(result.columns[0], col);
         let ecl_lon = get_float(result.data[0].get(&col).unwrap()).unwrap();
-        assert!(ecl_lon > 10.0 && ecl_lon < 20.0);
+        assert!(ecl_lon >= 10.0);
+    }
+
+    #[test]
+    fn request_less_than_condition() {
+        let col = Col::ecl_lon;
+        let cond = GaiaCondition::LessThan(col, 10.0);
+        let result = GaiaQueryBuilder::new(gaiadr3, gaia_source)
+            .top(NUMBER_OF_COLUMNS)
+            .select(vec![col])
+            .where_clause(cond)
+            .do_query()
+            .unwrap();
+        assert_eq!(result.columns[0], col);
+        let ecl_lon = get_float(result.data[0].get(&col).unwrap()).unwrap();
+        assert!(ecl_lon < 10.0);
+    }
+
+    #[test]
+    fn request_less_than_or_equal_condition() {
+        let col = Col::ecl_lon;
+        let cond = GaiaCondition::LessThanOrEqual(col, 10.0);
+        let result = GaiaQueryBuilder::new(gaiadr3, gaia_source)
+            .top(NUMBER_OF_COLUMNS)
+            .select(vec![col])
+            .where_clause(cond)
+            .do_query()
+            .unwrap();
+        assert_eq!(result.columns[0], col);
+        let ecl_lon = get_float(result.data[0].get(&col).unwrap()).unwrap();
+        assert!(ecl_lon <= 10.0);
     }
 }
