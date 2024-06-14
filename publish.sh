6b2dc98447fdfeb7080a92e336880a27c3b1b900
TheComamba <thecomamba@jojoheinze.de>
2024-06-14 16:20:34 +0200

    Improving publish script.

--- publish.sh
+++ publish.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+if [ ! -f ~/.cargo/credentials.toml ]
+then
+    echo "You are not logged in to cargo."
+    echo "Please visit "
+    echo "https://crates.io/me"
+    echo "to generate a token with the publish-update capability, and then run "
+    echo "cargo login <your-api-token>"
+    echo "to log in."
+    exit 1
+fi
+
 cargo install cargo-release
 
 if [[ "$1" == "--execute" ]]; then
